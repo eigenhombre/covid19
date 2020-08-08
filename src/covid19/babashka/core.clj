49b62766211a41db15541f48e29d3e2d259d718d
John Jacobsen <eigenhombre@gmail.com>
2020-08-08 12:01:08 -0500

    Accept any locale.

--- src/covid19/babashka/core.clj
+++ src/covid19/babashka/core.clj
@@ -30,10 +30,10 @@
           (normalize-records rawdata)
           filters))
 
-(defn cook-cases []
-  (selected-rows (comp (partial = "US") :Country_Region)
-                 (comp (partial = "Illinois") :Province_State)
-                 (comp (partial = "Cook") :Admin2)))
+(defn selected-cases [{:keys [region state locale]}]
+  (selected-rows (comp (partial = region) :Country_Region)
+                 (comp (partial = state) :Province_State)
+                 (comp (partial = locale) :Admin2)))
 
 (defn table-str [rows]
   (let [ncols (count (first rows))
@@ -51,31 +51,33 @@
                       (format (format "%%%ds" (nth col-lengths c))
                               (nth r c "?"))))))))
 
-(defn -main [& _]
-  (let [daily-cases
-        (->> (cook-cases)
-             first
-             :days
-             (map int))
-        weeks (partition-all 7 daily-cases)
-        weekly-deltas (map (comp (partial apply -)
-                                 (juxt last first))
-                           weeks)]
-    (println "Data starts 2020-01-22; today's date:" (str (LocalDate/now)))
-    (println "Weeks:")
-    (println (table-str (concat [["W" "Th" "F" "Sa" "Su" "M" "Tu"]
-                                 ["--" "--" "--" "--" "--" "--" "--"]]
-                                weeks)))
-    (println "\nSeven day average of daily increase:\n")
-    (println (table-str (concat [["W" "Th" "F" "Sa" "Su" "M" "Tu"]
-                                 ["--" "--" "--" "--" "--" "--" "--"]]
-                                (->> daily-cases
-                                     (partition 2 1)
-                                     (map (comp (partial apply -) reverse))
-                                     (partition 7 1)
-                                     (map (comp int #(/ % 7) (partial apply +)))
-                                     (partition-all 7)))))
-    (println "\nWeekly new:\n")
-    (println (->> weekly-deltas
-                  (partition-all 4)
-                  table-str))))
+(defn -main [& [region state locale]]
+  (if-not (and region state locale)
+    (println "Usage: covid19 <region-or-country> <province-or-state> <county-or-locale>")
+    (let [daily-cases
+          (->> (selected-cases {:region region, :state state, :locale locale})
+               first
+               :days
+               (map int))
+          weeks (partition-all 7 daily-cases)
+          weekly-deltas (map (comp (partial apply -)
+                                   (juxt last first))
+                             weeks)]
+      (println "Data starts 2020-01-22; today's date:" (str (LocalDate/now)))
+      (println "Weeks:")
+      (println (table-str (concat [["W" "Th" "F" "Sa" "Su" "M" "Tu"]
+                                   ["--" "--" "--" "--" "--" "--" "--"]]
+                                  weeks)))
+      (println "\nSeven day average of daily increase:\n")
+      (println (table-str (concat [["W" "Th" "F" "Sa" "Su" "M" "Tu"]
+                                   ["--" "--" "--" "--" "--" "--" "--"]]
+                                  (->> daily-cases
+                                       (partition 2 1)
+                                       (map (comp (partial apply -) reverse))
+                                       (partition 7 1)
+                                       (map (comp int #(/ % 7) (partial apply +)))
+                                       (partition-all 7)))))
+      (println "\nWeekly new:\n")
+      (println (->> weekly-deltas
+                    (partition-all 4)
+                    table-str)))))
